a39a21a0627ea7b1d69a5069ce625b651986df60
Ryan Deming <ryan@ryandeming.com>
2019-09-04 23:16:49 -0400

    first commit

--- resources/views/modules/fc-hero.blade.php
+++ resources/views/modules/fc-hero.blade.php
@@ -1,1 +1,5 @@
-<hero title="{{ get_sub_field('title') }}" text_area="{{ get_sub_field('text_area') }}" background_image="{{ get_sub_field('background_image') }}"></hero>
+    <hero
+    title="{{ get_sub_field('title') }}"
+    text_area="{{ get_sub_field('text_area') }}"
+    background_image="{{ get_sub_field('background_image') }}">
+    </hero>
